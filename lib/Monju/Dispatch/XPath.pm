aa8fd5f4c81d79de9f2fe78cbac8d0afcfe55c94
Yuval Kogman <nothingmuch@woobling.org>
2006-05-26 13:18:03 +0000

    stub for Dispatch::XPath

---
+++ lib/Monju/Dispatch/XPath.pm
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+
+package Monju::Dispatch::XPath;
+
+use strict;
+use warnings;
+
+
+
+__PACKAGE__;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Monju::Dispatch::XPath - Tree::XPathEngine based dispatching (TODO)
+
+=head1 SYNOPSIS
+
+	use Monju::Dispatch::XPath;
+
+=head1 DESCRIPTION
+
+=cut
+
+
