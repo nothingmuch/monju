675ab8985fe42e588faf740df20d2b7d4b3c4095
Yuval Kogman <nothingmuch@woobling.org>
2006-05-26 13:16:53 +0000

    Various ammendments + fixes for previous changes

---
+++ lib/Monju/Match/CollectedByPath.pm
@@ -0,0 +1,58 @@
+#!/usr/bin/perl
+
+package Monju::Match::CollectedByPath;
+use Moose;
+
+use strict;
+use warnings;
+
+use Scalar::Util qw/refaddr/;
+
+with qw/Monju::Match/; # whatever that is
+
+has sub_dispatches => (
+    isa => "ArrayRef",
+    is  => "ro",
+    auto_deref => 1,
+    required   => 1
+);
+
+has collected_matches => (
+    isa => "HashRef",
+    accessor => "_collected_matches",
+    required => 1,
+);
+
+has match => ( # the original match
+    is => "ro",
+);
+
+sub matches_for_dispatches {
+    my $self = shift;
+    @{ $self->_collected_matches }{ map { refaddr($_) } $self->sub_dispatches }; # ordered correctly
+}
+
+sub matches_for_dispatch {
+    my ( $self, $dispatch ) = @_;
+    @{ $self->_collected_matches->{ refaddr($dispatch) } };
+}
+
+__PACKAGE__;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Monju::Match::CollectedByPath - 
+
+=head1 SYNOPSIS
+
+	use Monju::Match::CollectedByPath;
+
+=head1 DESCRIPTION
+
+=cut
+
+
---
+++ lib/Moose/Meta/Role/SkipBuild.pm
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+package Moose::Meta::Role::SkipBuild;
+use Moose;
+
+use Moose::Role ();
+
+use strict;
+use warnings;
+
+use Context::Handle qw/context_sensitive/;
+
+sub import {
+    my ( $class, @import ) = @_;
+
+    no strict 'refs';
+    my $meta = __PACKAGE__->new( role_name => scalar caller );
+    *{ caller() . "::meta" } = sub { $meta };
+
+    shift @_;
+    unshift @_, "Moose::Role";
+    goto \&Moose::Role::import;
+}
+
+extends qw/Moose::Meta::Role/;
+
+around _check_required_methods => sub {
+    my $next = shift;
+    my ( $self, @args ) = @_;
+
+    my $rv = context_sensitive { eval { $self->$next( @args ) } };
+    die $@ if $@ and $@ !~ /BUILD/;
+    $rv->return;
+};
+
+__PACKAGE__;
+
+__END__
+
+=pod
+
+=head1 NAME
+
+Moose::Meta::Role::SkipBuild - 
+
+=head1 SYNOPSIS
+
+	use Moose::Meta::Role::SkipBuild;
+
+=head1 DESCRIPTION
+
+=cut
+
+
